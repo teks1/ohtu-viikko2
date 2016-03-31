258dec6b76dd65853b6166f1d2c2c40b14219289
teks1 <marko.vuorenmaa@cs.helsinki.fi>
2016-03-31 23:57:12 +0300

    viikko 2, teht 11

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -1,12 +1,16 @@
 package ohtu.verkkokauppa;
 
 import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Varasto implements VarastoInterface {
 
     private Kirjanpito kirjanpito;
     private HashMap<Tuote, Integer> saldot;
 
+    @Autowired
     public Varasto(Kirjanpito kirjanpito) {
         this.kirjanpito = kirjanpito;
         this.saldot = new HashMap<Tuote, Integer>();
