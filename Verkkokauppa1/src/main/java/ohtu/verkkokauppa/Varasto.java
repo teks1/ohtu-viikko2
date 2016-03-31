db9066bea32bd59ac090da1b3e551b0524c2cf01
teks1 <marko.vuorenmaa@cs.helsinki.fi>
2016-03-31 23:13:37 +0300

    viikko 2, teht 9

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -4,51 +4,43 @@ import java.util.*;
 
 public class Varasto implements VarastoInterface {
 
-    private static Varasto instanssi;
-
-    public static Varasto getInstance() {
-        if (instanssi == null) {
-            instanssi = new Varasto();
-        }
-
-        return instanssi;
-    }
-    
     private Kirjanpito kirjanpito;
-    private HashMap<Tuote, Integer> saldot;  
-    
-    private Varasto() {
-        kirjanpito = Kirjanpito.getInstance();
-        saldot = new HashMap<Tuote, Integer>();
+    private HashMap<Tuote, Integer> saldot;
+
+    public Varasto(Kirjanpito kirjanpito) {
+        this.kirjanpito = kirjanpito;
+        this.saldot = new HashMap<Tuote, Integer>();
         alustaTuotteet();
     }
-            
+
     @Override
-    public Tuote haeTuote(int id){
+    public Tuote haeTuote(int id) {
         for (Tuote t : saldot.keySet()) {
-            if ( t.getId()==id) return t;
+            if (t.getId() == id) {
+                return t;
+            }
         }
-        
+
         return null;
     }
 
     @Override
-    public int saldo(int id){
+    public int saldo(int id) {
         return saldot.get(haeTuote(id));
     }
-    
+
     @Override
-    public void otaVarastosta(Tuote t){        
-        saldot.put(t,  saldo(t.getId())-1 );
-        kirjanpito.lisaaTapahtuma("otettiin varastosta "+t);
+    public void otaVarastosta(Tuote t) {
+        saldot.put(t, saldo(t.getId()) - 1);
+        kirjanpito.lisaaTapahtuma("otettiin varastosta " + t);
     }
-    
+
     @Override
-    public void palautaVarastoon(Tuote t){
-        saldot.put(t,  saldo(t.getId())+1 );
-        kirjanpito.lisaaTapahtuma("palautettiin varastoon "+t);
-    }    
-    
+    public void palautaVarastoon(Tuote t) {
+        saldot.put(t, saldo(t.getId()) + 1);
+        kirjanpito.lisaaTapahtuma("palautettiin varastoon " + t);
+    }
+
     private void alustaTuotteet() {
         saldot.put(new Tuote(1, "Koff Portteri", 3), 100);
         saldot.put(new Tuote(2, "Fink Bräu I", 1), 25);
