56c5aa26eaba94ddec95024ed33528dfd80fc627
teks1 <marko.vuorenmaa@cs.helsinki.fi>
2016-04-01 17:22:59 +0300

    viikko 3, teht 1

--- Verkkokauppa1/src/main/java/ohtu/Main.java
+++ Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -2,9 +2,6 @@ package ohtu;
 
 import ohtu.verkkokauppa.Kauppa;
 import ohtu.verkkokauppa.Kirjanpito;
-import ohtu.verkkokauppa.Pankki;
-import ohtu.verkkokauppa.Varasto;
-import ohtu.verkkokauppa.Viitegeneraattori;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
@@ -17,24 +14,35 @@ public class Main {
         Kauppa kauppa = (Kauppa) ctx.getBean("kauppa");
 
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
+        hoidaAsiakas(kauppa);
+
+        // seuraava asiakas
+        seuraavaAsiakas(kauppa);
+        // kirjanpito
+        tulostaKirjanpito(kirjanpito);
+    }
+
+    public static void hoidaAsiakas(Kauppa kauppa) {
         kauppa.aloitaAsiointi();
         kauppa.lisaaKoriin(1);
         kauppa.lisaaKoriin(3);
         kauppa.lisaaKoriin(3);
         kauppa.poistaKorista(1);
         kauppa.tilimaksu("Pekka Mikkola", "1234-12345");
+    }
 
-        // seuraava asiakas
+    public static void tulostaKirjanpito(Kirjanpito kirjanpito) {
+        for (String tapahtuma : kirjanpito.getTapahtumat()) {
+            System.out.println(tapahtuma);
+        }
+    }
+
+    public static void seuraavaAsiakas(Kauppa kauppa) {
         kauppa.aloitaAsiointi();
         for (int i = 0; i < 24; i++) {
             kauppa.lisaaKoriin(5);
         }
 
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
-
-        // kirjanpito
-        for (String tapahtuma : kirjanpito.getTapahtumat()) {
-            System.out.println(tapahtuma);
-        }
     }
 }
